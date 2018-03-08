734d949b5955eb32d8d932c95de2387966ef512a
AliTon <tonoyanalbert94@gmail.com>
2018-03-09 00:43:00 +0400

    add function constructor for user object

--- user.js
+++ user.js
@@ -1,19 +1,22 @@
-    const list = document.getElementById('demo');
 
-     save = () => {
-            const firstname = document.getElementById('firstname').value;
-            const lastname = document.getElementById('lastname').value;
-         let user = {
-             firstname: firstname,
-             lastname: lastname
-         }
-            const entry = document.createElement('li');
-         
+function User (firstname, lastname) {
+    this.firstName = firstname;
+    this.lastName = lastname;
+};
+     
+save = () => {
+        const entry = document.createElement('li');
+        const firstname = document.getElementById('firstname').value;
+        const lastname = document.getElementById('lastname').value;
+        const list = document.getElementById('demo');
+
+    
          if(firstname !== "" && lastname !== ""){
-                entry.appendChild(document.createTextNode(user.firstname+"  "+user.lastname));
+                const newUser = new User(firstname,lastname);
+                entry.appendChild(document.createTextNode(newUser.firstName+"  "+newUser.lastName));
                 list.appendChild(entry);
                 document.getElementById("firstname").value ="";
                 document.getElementById("lastname").value ="";
             } else 
                 alert('Please enter name/username');
-        }
+     };
