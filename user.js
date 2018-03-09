35ccf136a9ea61649c188997c4fd4ba1b6453cea
AliTon <tonoyanalbert94@gmail.com>
2018-03-09 16:45:45 +0400

    users list with delete button

--- user.js
+++ user.js
@@ -4,19 +4,28 @@ function User (firstname, lastname) {
     this.lastName = lastname;
 };
      
-let save = () => {
-        const entry = document.createElement('li');
-        const firstname = document.getElementById('firstname').value;
-        const lastname = document.getElementById('lastname').value;
-        const list = document.getElementById('demo');
-
+let add =  save = () => {
+        const entry = document.createElement('li'),
+              del = document.createElement('button'),
+              delBtn = document.createTextNode("delete"),
+              firstname = document.getElementById('firstname').value,
+              lastname = document.getElementById('lastname').value,
+              list = document.getElementById('demo'),
+              val = ++this.val;
     
          if(firstname !== "" && lastname !== ""){
-                const newUser = new User(firstname,lastname);
+                const newUser = new User(firstname,lastname,this.val);
                 entry.appendChild(document.createTextNode(newUser.firstName+"  "+newUser.lastName));
                 list.appendChild(entry);
+                del.appendChild(delBtn);
+                list.appendChild(del);
                 document.getElementById("firstname").value ="";
                 document.getElementById("lastname").value ="";
-            } else 
-                alert('Please enter name/username');
+             
+                del.onclick = () => {
+                        list.removeChild(entry);
+                        list.removeChild(del) ;
+                     };
+            } 
+              else  alert('Please enter name/username');
      };
